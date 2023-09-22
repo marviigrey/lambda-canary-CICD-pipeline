1211db99a38da9b215d14c790e4f7dd4ec5bffdf
marviigrey <marvellousfrederick@gmail.com>
2023-09-22 02:52:05 +0000

    environment is set

---
+++ CDK/cdk_workshop/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from aws_cdk import App, Environment
+from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
+
+app = App()
+
+environment_type = app.node.try_get_context("environmentType")
+environment_context = app.node.try_get_context(environment_type)
+region = environment_context["region"]
+account = app.node.try_get_context("account")
+tags = environment_context["tags"]
+stack_name = f'{app.node.try_get_context("prefix")}-{environment_type}'
+
+CdkWorkshopStack(
+    app,
+    stack_name,
+     env = Environment(
+        account = account,
+        region = region
+    ),
+    tags=tags,
+)
+
+app.synth()
+
